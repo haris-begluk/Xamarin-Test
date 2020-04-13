60b9fb4fe9500ee733aad5068824f1cf6f94ae40
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-04-13 13:17:42 +0200

    Property element syntax

--- Hamer/Hamer/Views/GreetPage.xaml.cs
+++ Hamer/Hamer/Views/GreetPage.xaml.cs
@@ -18,46 +18,7 @@ namespace Hamer.Views
             slider.Value = 0.5;
 
 
-            switch (Device.RuntimePlatform)
-            {
-                case Device.iOS:
-                    this.Padding = new Thickness(0, 20, 0, 0);
-                    break;
-                case Device.Android:
-                case Device.UWP:
-                case Device.macOS:
-                default:
-                    this.Padding = new Thickness(0);
-                    break;
-            }
 
-
-            //Padding = Device.OnPlatform(
-            //     iOS: new Thickness(0, 20, 0, 0),
-            //     Android: new Thickness(20, 20, 20, 20),
-            //     WinPhone: new Thickness(20, 0, 20, 20));
-
-            //Device.OnPlatform(iOS: () =>
-            //{
-            //    Padding = new Thickness(0, 20, 0, 0);
-            //}, Android: () =>
-            //{
-            //    Padding = new Thickness(0, 20, 0, 0);
-            //});
-
-
-            //if (Device.RuntimePlatform.Equals(Device.iOS)) //Setting
-            //{
-            //    Padding = new Thickness(0, 20, 0, 0);
-            //}
-            //else if (Device.RuntimePlatform.Equals(Device.Android))
-            //{
-            //    Padding = new Thickness(20, 20, 20, 20);
-            //}
-            //else if (Device.RuntimePlatform.Equals(Device.UWP))
-            //{
-            //    Padding = new Thickness(20, 0, 20, 20);
-            //}
 
         }
 
