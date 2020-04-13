730b97f8a2860e72e5368fbd91998f07d4169fd7
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-04-13 14:25:36 +0200

    Stack layout in code

--- Hamer/Hamer/Views/StackPage.xaml.cs
+++ Hamer/Hamer/Views/StackPage.xaml.cs
@@ -15,6 +15,14 @@ namespace Hamer.Views
         public StackPage()
         {
             InitializeComponent();
+            var layout = new StackLayout
+            {
+                Spacing = 40,
+                Padding = new Thickness(0, 20, 0, 0),
+                Orientation = StackOrientation.Horizontal
+            };
+            layout.Children.Add(new Label { Text = "Label 1" });
+            Content = layout;
         }
     }
 }
